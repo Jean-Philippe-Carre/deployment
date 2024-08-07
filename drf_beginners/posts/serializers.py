4e3c8d3f3a1e22ea6582abd666344ae34e5e4b13
Jean-Philippe-Carre <jp.carre@hotmail.fr>
2024-08-07 11:54:45 +0400

    deployment

--- drf_beginners/posts/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Post
-
-
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = ('custom_id', 'title', 'category', 'content',
-                  'publish_date', 'last_updated')
